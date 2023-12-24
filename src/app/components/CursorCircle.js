a432bbd823f59fd19f09cd62f0673a207d794913
Vedant <vedantkingh@gmail.com>
2023-12-24 20:41:51 +0530

    UI

--- src/app/components/CursorCircle.js
+++ src/app/components/CursorCircle.js
@@ -1,10 +1,96 @@
-// import React from 'react';
-// import styles from './VibratingPudding.module.css';
+// components/JigglyCursor.js
+import React, { useEffect, useRef } from 'react';
+import AudioPlayer from 'react-audio-player';
+import p5 from 'p5';
 
-// const CursorCircle = ({ isPlaying }) => {
-//   const puddingClass = isPlaying ? styles.vibrating : '';
+const CursorCircle = ({ audioSrc }) => {
+  const cursorRef = useRef(null);
 
-//   return <div className={`${styles.pudding} ${puddingClass}`}></div>;
+  useEffect(() => {
+    const sketch = (p) => {
+      let jiggle = 0;
+
+      p.setup = () => {
+        const canvas = p.createCanvas(p.windowWidth, p.windowHeight);
+        canvas.position(0, 0);
+        canvas.style('pointer-events', 'none');
+        cursorRef.current = p.createDiv('');
+        cursorRef.current.style('position', 'absolute');
+      };
+
+      p.draw = () => {
+        p.background(255);
+        cursorRef.current.style('left', p.mouseX + 'px');
+        cursorRef.current.style('top', p.mouseY + 'px');
+        cursorRef.current.style('transform', `rotate(${jiggle}deg)`);
+        jiggle += p.random(-1, 1);
+      };
+
+      p.windowResized = () => {
+        p.resizeCanvas(p.windowWidth, p.windowHeight);
+      };
+
+      p.playJiggle = () => {
+        jiggle = 20; // Adjust the initial jiggle amount based on your preference
+      };
+    };
+
+    new p5(sketch, cursorRef.current);
+
+    return () => {
+      // Cleanup function
+      cursorRef.current.remove();
+    };
+  }, []);
+
+  return (
+    <div>
+      <AudioPlayer
+        src={audioSrc}
+        autoPlay={false}
+        controls
+        onPlay={() => cursorRef.current.playJiggle()}
+      />
+    </div>
+  );
+};
+
+export default CursorCircle;
+
+
+
+
+
+
+// // JigglyCursor.js
+// "use client";
+// import React, { useState, useEffect } from 'react';
+// import { Howl } from 'react-howler';
+// import { Analyser } from 'react-audio-analyser';
+// import styles from './CursorCircle.module.css';
+
+// const CursorCircle = () => {
+//   const [isPlaying, setIsPlaying] = useState(false);
+
+//   const handleTogglePlay = () => {
+//     setIsPlaying(!isPlaying);
+//   };
+
+//   return (
+//     <div className={styles.container}>
+//       <Analyser
+//         player={<Howl src="/music.mp3" playing={isPlaying} />}
+//         fftSize={256}
+//         barStyle={{ width: '2px', backgroundColor: '#ec4e39' }}
+//       />
+//       <div
+//         className={`${styles.cursor} ${isPlaying ? styles.jiggly : ''}`}
+//       ></div>
+//       <button onClick={handleTogglePlay} className={styles.playButton}>
+//         {isPlaying ? 'Pause' : 'Play'}
+//       </button>
+//     </div>
+//   );
 // };
 
 // export default CursorCircle;
--- src/app/page.js
+++ src/app/page.js
@@ -2,117 +2,29 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 import common from './styles/common.module.css'
-import CursorCircle from '@/app/components/CursorCircle';
-import AudioAnalyser from 'react-audio-analyser';
+// import CursorCircle from '@/app/components/CursorCircle';
+import useMousePosition from '@/app/components/useMousePosition';
+import CursorCircle from './components/CursorCircle';
+// import AudioAnalyser from 'react-audio-analyser';
 
 export default function Home() {
-
-  // const [isPlaying, setIsPlaying] = useState(false);
-
-  // const handleAudioProcess = (data) => {
-  //   // You can use the audio data to determine if music is playing
-  //   const isMusicPlaying = data.volume > 0.1; // Adjust the threshold as needed
-
-  //   setIsPlaying(isMusicPlaying);
-  // };
-
-  // useEffect(() => {
-  //   // Set up audio playback here if needed
-  // }, []);
+  // const [isHovered, setIsHovered] = useState(false);
+  // const { x, y } = useMousePosition();
 
   return (
     <main className={styles.main}>
-      {/* <AudioAnalyser
-        audio={'/music.mp3'} // Replace with your actual audio source
-        onAnalyser={handleAudioProcess}
-      /> */}
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {/* <motion.div 
+        className={common.mask}
+        animate={{
+          x: x - 45, // Adjust the values as needed
+          y: y - 145, // Adjust the values as needed
+        }}
+        transition={{ type: "tween", ease: "backOut", duration: 0.5 }}
+      ></motion.div> */}
+      <h1>Your Next.js App</h1>
+      <CursorCircle audioSrc="/music.mp3" />
     </main>
   )
 }
