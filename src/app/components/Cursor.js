0b8f7cda31f94e050613eea81b2e9871c2d2c7b3
Vedant <vedantkingh@gmail.com>
2024-01-16 12:06:44 +0530

    Bubble moving with cursor

--- src/app/components/Cursor.js
+++ src/app/components/Cursor.js
@@ -3,14 +3,15 @@
 import * as THREE from 'three';
 import { useEffect, useRef, useState } from 'react';
 import styles from '@/app/styles/common.module.css';
+import { createNoise2D, createNoise3D } from 'simplex-noise';
 
 const Cursor = () => {
     const [position, setPosition] = useState({ x: 0, y: 0 });
-    const [speed, setSpeed] = useState(50.0);
-    const [spikes, setSpikes] = useState(0.8);
-    const [processing, setProcessing] = useState(0.8);
+    const [speed, setSpeed] = useState(60.0);
+    const [spikes, setSpikes] = useState(1);
+    const [processing, setProcessing] = useState(1);
 
-    const bubbleRef = useRef();
+    const bubbleRef = useRef(null);
 
     useEffect(() => {
         // Scene & Camera
@@ -20,8 +21,10 @@ const Cursor = () => {
 
         // Renderer
         const renderer = new THREE.WebGLRenderer({ alpha: true });
+        renderer.setPixelRatio(2);
         renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild(renderer.domElement);
+        const container = bubbleRef.current;
+        container.appendChild(renderer.domElement);
 
         // Bubble
         const bubbleGeometry = new THREE.CircleGeometry(20, 128, 6, 6.3);
@@ -59,28 +62,37 @@ const Cursor = () => {
 
         window.addEventListener('resize', handleResize);
 
+        const noise = createNoise3D();
+
         const animate = () => {
             const time = performance.now() * 0.00001 * speed * Math.pow(processing, 3);
 
             const positions = bubble.geometry.attributes.position.array;
+            let scale;
 
             for (let i = 0; i < positions.length; i += 3) {
                 const vertex = new THREE.Vector3(positions[i], positions[i + 1], positions[i + 2]);
 
                 // Update vertex positions based on noise functions
-                vertex.normalize().multiplyScalar(1 + 0.3 * Math.sin(vertex.x * spikes) * Math.sin(vertex.y * spikes + time));
+                const noiseValue = noise(vertex.x * 0.1, vertex.y * 0.1, time * 0.1) * Math.sin(vertex.x * spikes) * Math.sin(vertex.y * spikes + time);
+                scale = 1 + 0.3 * noiseValue;
+                vertex.normalize().multiplyScalar(scale);
+                // vertex.normalize().multiplyScalar(1 + 0.3 * Math.sin(vertex.x * spikes) * Math.sin(vertex.y * spikes + time));
+
 
                 positions[i] = vertex.x;
                 positions[i + 1] = vertex.y;
                 positions[i + 2] = vertex.z;
             }
 
-            bubble.geometry.attributes.position.needsUpdate = true;
-            const mouseX = (position.x / window.innerWidth) * 2 - 1;
-            const mouseY = -(position.y / window.innerHeight) * 2 + 1;
+            bubble.scale.set(scale, scale, scale);
 
-            const mouseVector = new THREE.Vector3(mouseX, mouseY, 0);
-            mouseVector.unproject(camera);
+            bubble.geometry.attributes.position.needsUpdate = true;
+            // const mouseX = (position.x / window.innerWidth) * 2 - 1;
+            // const mouseY = -(position.y / window.innerHeight) * 2 + 1;
+
+            const mouseVector = new THREE.Vector3(position.x, position.y, 0);
+            // mouseVector.unproject(camera);
 
             bubble.position.set(mouseVector.x, mouseVector.y, 0); 
 
@@ -92,19 +104,26 @@ const Cursor = () => {
 
         animate();
         const handleMouseMove = (event) => {
-            setPosition({ x: event.clientX, y: event.clientY });
+            // setPosition({ x: event.clientX, y: event.clientY });
+            const mouseX = ( event.clientX / window.innerWidth ) * 2 - 1;
+            const mouseY = - ( event.clientY / window.innerHeight ) * 2 + 1;
+            const fovHalfRadians = Math.tan (Math.PI / 180 * 75 / 2);
+            position.x = mouseX * camera.position.z * fovHalfRadians ;
+            position.y = mouseY * camera.position.z * fovHalfRadians / 2;
         };
-        window.addEventListener('mousemove', handleMouseMove);
+        window.addEventListener('mousemove', handleMouseMove, false);
 
 
         // Cleanup
-        return () => {
-            window.removeEventListener('resize', handleResize);
-            window.removeEventListener('mousemove', handleMouseMove);
-        };
+        // return () => {
+        //     window.removeEventListener('resize', handleResize);
+        //     window.removeEventListener('mousemove', handleMouseMove);
+        // };
     }, [speed, spikes, processing]); // Re-run when speed, spikes, or processing change
 
-    return null;
+    return (
+        <div ref={bubbleRef} className={styles.webgl}></div>
+    );
 };
 
 export default Cursor;
